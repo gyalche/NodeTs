e255cff69ca54a71422e869413db4d36bedf1199
dawa sherpa <sherpadawag1998@gmail.com>
2023-08-06 21:48:38 +0545

    user model created

---
+++ src/model/user.model.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import config from 'config';
+
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const user = mongoose.model('user', userSchema);
+
+export default user;
