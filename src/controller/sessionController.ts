ebbd159ed4d3f9bc98d262c7a5d343d2997556f5
dawa sherpa <sherpadawag1998@gmail.com>
2023-08-29 23:22:46 +0545

    woring on oAuth

--- src/controller/sessionController.ts
+++ src/controller/sessionController.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from 'express';
+import { validatePassword } from '../service/user.service';
 
 export async function createUserSessionHandler(req: Request, res: Response) {
   //validate user password
   const user = validatePassword();
+  if (!user) {
+    return res.status(404).send('Invalid email and password');
+  }
   //create session;
+  const session = createSession(user.id, user.get('user-agent') || '');
   //create an access token;
   //create a refresh token;
   //return access and refresh token;
--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,5 +1,8 @@
 import { Express, Request, Response } from 'express';
-import { createUserHandler } from '../controller/userController';
+import {
+  createUserHandler,
+  getGoogleOauthHandler,
+} from '../controller/userController';
 import validate from '../middleware/validateResource';
 import { createUserSchema } from '../schema/user.schema';
 function routes(app: Express) {
@@ -7,5 +10,7 @@ function routes(app: Express) {
     return res.sendStatus(200);
   });
   app.post('/api/users', validate(createUserSchema), createUserHandler);
+
+  app.post('/api/session/oauth/google', getGoogleOauthHandler);
 }
 export default routes;
